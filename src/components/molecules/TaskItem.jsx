a5d2003d19e4a8db8cb82a49d5b6002480ecc1ad
Ali Nurdin <alinurdin.dev@gmail.com>
2024-04-29 15:42:31 +0700

    fix responsive

--- src/components/molecules/TaskItem.jsx
+++ src/components/molecules/TaskItem.jsx
@@ -9,31 +9,33 @@ export default function TaskItem({ task, toggleTask, deleteTask }) {
   return (
     <div className="card border-2 w-full hover:scale-[1.1] duration-500">
       <div className="card-body">
-        <div className="flex justify-between align-center gap-1">
+        <div className="flex flex-col lg:flex-row justify-between align-center gap-1">
           <hgroup>
             <h1 className="text-4xl font-bold">{task.title}</h1>
             <div className="flex gap-5 mt-3">
-              <span className="text-sm">Due data: {task.date}</span>
-              <span className={`text-sm duration-500 font-semibold ${task.is_completed ? 'text-success' : 'text-error'}`}>{task.is_completed ? 'Complete' : 'Not Complete'}</span>
+              <span className="text-xs md:btn-sm">Due data: {task.date}</span>
+              <span className={`text-xs md:btn-sm duration-500 font-semibold ${task.is_completed ? 'text-success' : 'text-error'}`}>{task.is_completed ? 'Complete' : 'Not Complete'}</span>
             </div>
           </hgroup>
-          <button
-            className="btn my-auto ml-auto btn-sm btn-success"
-            onClick={() => navigate(`/task/${task.id}/edit`, {
-              state: task
-            })}
-          >
-            <Icon icon={faPencilAlt} />
-          </button>
-          <button
-            className="btn my-auto btn-sm btn-error"
-            onClick={deleteTask}
-          >
-            <Icon icon={faTrashAlt} />
-          </button>
-          <button disabled={task.is_completed} className={`btn my-auto btn-sm ${task.is_completed ? 'btn-warning' : 'btn-primary'}`} onClick={toggleTask}>
-            {task.is_completed ? <Icon icon={faX} /> : <Icon icon={faCheck} />}
-          </button>
+          <div className="my-auto flex gap-1">
+            <button
+              className="btn btn-sm btn-success"
+              onClick={() => navigate(`/task/${task.id}/edit`, {
+                state: task
+              })}
+            >
+              <Icon icon={faPencilAlt} />
+            </button>
+            <button
+              className="btn btn-sm btn-error"
+              onClick={deleteTask}
+            >
+              <Icon icon={faTrashAlt} />
+            </button>
+            <button disabled={task.is_completed} className={`btn my-auto btn-sm ${task.is_completed ? 'btn-warning' : 'btn-primary'}`} onClick={toggleTask}>
+              {task.is_completed ? <Icon icon={faX} /> : <Icon icon={faCheck} />}
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- src/components/organisms/Layout.jsx
+++ src/components/organisms/Layout.jsx
@@ -4,7 +4,7 @@ export default function Layout() {
   const navigate = useNavigate()
 
   return (
-    <div className="container mx-auto p-24">
+    <div className="container mx-auto p-6 md:p-24">
       <header>
         <nav className="flex gap-3">
           <button onClick={() => navigate('/')} className="btn btn-primary">Home</button>
